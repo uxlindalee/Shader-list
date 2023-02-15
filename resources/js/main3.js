d80c4d37c08c9d555018471189c537ae5dc22c08
uxlindalee <uxlindalee@gmail.com>
2023-02-15 18:07:27 +0900

    update

--- resources/js/main3.js
+++ resources/js/main3.js
@@ -16,12 +16,8 @@ const threeProject = (() => {
 
 	const listCount = document.getElementsByClassName("listitem").length;
 	let listArray = [];
-	let currentShader = [];
 	let imagesList = [];
-	let boxGap = { x: 0.25, y: 0.503 };
-	const listColumn = 3;
-
-	let meshWidth, meshHeight, meshDepth;
+	let targetMesh = null;
 
 	initScrollTop = scrollY;
 
@@ -83,13 +79,14 @@ const threeProject = (() => {
 	// })
 
 	const setTheLayout = () => {
-		geometry = new THREE.PlaneGeometry(1.33, 1, 32, 32);
+		geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 		material = new THREE.ShaderMaterial({
 			side: THREE.DoubleSide,
 			depthTest: false,
 			// wireframe: true,
 			uniforms: {
 				u_pixels: { value: new THREE.Vector2(areaWidth, areaHeight) },
+				u_alpha: { value: 1.0 },
 				u_position: { value: new THREE.Vector2(0.0, 0.0) },
 				u_accel: { value: new THREE.Vector2(0.5, 2.0) },
 				u_progress: { value: 0 },
@@ -141,6 +138,7 @@ const threeProject = (() => {
 			fragmentShader: `
         uniform float u_time;
         uniform float u_progress;
+        uniform float u_alpha;
         uniform sampler2D u_texture1;
         uniform sampler2D u_texture2;
         uniform vec2 u_pixels;
@@ -183,7 +181,8 @@ const threeProject = (() => {
 
           vec4 rgba = mix(rgba1,rgba2,delayValue);
 
-          gl_FragColor = rgba;
+          gl_FragColor = vec4(rgba);
+          gl_FragColor.a = u_alpha;
         }
       `,
 			// color: "#555555",
@@ -202,72 +201,37 @@ const threeProject = (() => {
 		}
 	};
 
-	// const setTheModel = () => {
-	// 	gltfLoader = new GLTFLoader();
-	// url = "./";
-
-	// gltfLoader.load(url, (object) => {
-	//   model = object;
-	//   scene.add(model.scene);
-	// });
-
-	// 	renderRequest();
-	// };
-
 	const setTheSizing = () => {
-		const size = new THREE.Box3().setFromObject(mesh);
+		const listItems = document.querySelectorAll(".listitem img");
+		// let meshPosition = mesh.position;
+		// let cameraDistanceFromMesh = camera.position.distanceTo(meshPosition);
+		sceneUnit = 2 * (camera.position.z - listArray[0].position.z) * Math.tan(THREE.MathUtils.degToRad(camera.fov / 2));
+		sceneUnit /= areaHeight;
 
-		meshWidth = size.max.x - size.min.x;
-		meshHeight = size.max.y - size.min.y;
-		meshDepth = size.max.z - size.min.z;
+		const scrollTop = window.pageYOffset;
+		listArray.forEach((mesh, index) => {
+			const rect = listItems[index].getBoundingClientRect();
 
-		let meshPosition = mesh.position;
-		let cameraDistanceFromMesh = camera.position.distanceTo(meshPosition);
-		let targetHeight = meshHeight / 2;
+			mesh.scale.set(rect.width * sceneUnit, rect.height * sceneUnit, 1);
+			mesh.position.x = (rect.left + rect.width / 2 - areaWidth / 2) * sceneUnit;
+			mesh.position.y = -(rect.top + scrollTop + rect.height / 2 - areaHeight / 2) * sceneUnit;
+			mesh.userData.offsetY = -(rect.top + scrollTop + rect.height / 2 - areaHeight / 2) * sceneUnit;
+			mesh.userData.scale = mesh.scale.clone();
+			mesh.userData.position = mesh.position.clone();
+		});
+	};
 
-		cameraDistanceFromMesh -= meshDepth / 2;
-		camera.fov = (180 / Math.PI) * Math.atan(targetHeight / cameraDistanceFromMesh) * 2; //Cube 100% fit to the screen size
-		// camera.fov = (180 / Math.PI) * Math.atan(targetHeight / cameraDistanceFromMesh) * 2 * 4.85; //Cube 50% fit to the screen size
-
-		camera.updateProjectionMatrix();
-
-		meshArea = document.querySelector(".listitem img");
-		scale = meshArea.offsetHeight / areaHeight;
-
-		setViewOffsetFunction();
+	function onScroll() {
+		const scrollTop = window.pageYOffset;
 
 		for (let i = 0; i < listArray.length; i++) {
-			const item = listArray[i];
-
-			item.position.x = (meshWidth + boxGap.x) * scale * (i % listColumn);
-			item.position.y = -(meshHeight + boxGap.y) * scale * Math.floor(i / listColumn);
-			item.savePosition = item.position.clone();
-			item.scale.set(scale, scale);
+			const plane = listArray[i];
+			plane.position.y = plane.userData.offsetY + scrollTop * sceneUnit;
 		}
-	};
-
-	const setViewOffsetFunction = () => {
-		const firstImg = document.querySelectorAll(".listitem img")[0];
-		const meshRect = firstImg.getBoundingClientRect();
-
-		camera.setViewOffset(
-			areaWidth,
-			areaHeight,
-			areaWidth / 2 - meshRect.width / 2 - (meshRect.left - 0), // offset x
-			areaHeight / 2 - meshRect.height / 2 - (meshRect.top - 0), // offset y
-			areaWidth,
-			areaHeight
-		);
-	};
+	}
 
 	//Scroll
-	window.addEventListener("scroll", function () {
-		const scrollRatio = scrollY / areaHeight;
-
-		for (let i = 0; i < listArray.length; i++) {
-			listArray[i].position.y = listArray[i].savePosition.y + scrollRatio;
-		}
-	});
+	window.addEventListener("scroll", onScroll);
 
 	const setTheTexture = () => {
 		textureLoader = new THREE.TextureLoader(loadingManager);
@@ -276,104 +240,171 @@ const threeProject = (() => {
 	const setTheRaycaster = () => {
 		const raycaster = new THREE.Raycaster();
 		const pointer = new THREE.Vector2();
-		let currentMesh;
+		let scaleUnit = 1.2;
 
-		document.addEventListener("mousemove", function (e) {
+		const popupMesh = document.querySelectorAll(".listitem a");
+		for (let i = 0; i < popupMesh.length; i++) {
+			popupMesh[i].addEventListener("mouseenter", enterMesh);
+		}
+
+		function enterMesh(e) {
+			if (targetMesh) return;
+
+			const targetElement = e.target.tagName !== "LI" ? e.target.closest("li") : e.target;
+			const parentNode = [...targetElement.parentElement.children];
+			const targetIndex = parentNode.indexOf(targetElement);
+
+			const plane = listArray[targetIndex];
+			const planeProgressUniform = plane.material.uniforms.u_progress;
+
+			plane.renderOrder = 1;
+			plane.material.uniforms.u_alpha.value = 1.0;
+
+			plane.userData.progressTween && plane.userData.progressTween.kill();
+			plane.userData.progressTween = gsap.to(planeProgressUniform, 0.75, {
+				value: 0.99999,
+				ease: "cubic.out",
+			});
+
+			plane.scale.hoverTween && plane.scale.hoverTween.kill();
+			plane.scale.hoverTween = gsap.to(plane.scale, 0.75, {
+				x: plane.userData.scale.x * scaleUnit,
+				y: plane.userData.scale.y * scaleUnit,
+				ease: "cubic.out",
+			});
+
+			targetElement.addEventListener("mousedown", clickMesh);
+			targetElement.addEventListener("mouseleave", leaveMesh);
+			targetElement.addEventListener("mousemove", moveMesh);
+		}
+
+		function clickMesh(e) {
+			document.body.style.overflow = "hidden";
+
+			const targetElement = e.target.tagName !== "LI" ? e.target.closest("li") : e.target;
+			const parentNode = [...targetElement.parentElement.children];
+			const targetIndex = parentNode.indexOf(targetElement);
+
+			const plane = listArray[targetIndex];
+
+			const planeHoverUniform = plane.material.uniforms.u_hover.value;
+			const planeProgressUniform = plane.material.uniforms.u_progress;
+
+			targetMesh = plane;
+
+			plane.position.hoverTween && plane.position.hoverTween.kill();
+			plane.position.hoverTween = gsap.to(plane.position, 0.75, {
+				x: 0,
+				y: 0,
+				z: 0,
+				ease: "cubic.out",
+			});
+
+			plane.scale.clickTween && plane.scale.clickTween.kill();
+			plane.scale.clickTween = gsap.to(plane.scale, 0.75, {
+				x: sceneUnit * areaHeight * 1.33,
+				y: sceneUnit * areaHeight,
+				ease: "cubic.out",
+			});
+
+			plane.userData.hoverTween && plane.userData.hoverTween.kill();
+			plane.userData.hoverTween = gsap.to(planeHoverUniform, 0.75, {
+				x: 0,
+				y: 0,
+				z: 0,
+				ease: "cubic.out",
+			});
+
+			plane.userData.progressTween && plane.userData.progressTween.kill();
+			plane.userData.progressTween = gsap.to(planeProgressUniform, 0.75, {
+				value: 0,
+				ease: "cubic.out",
+			});
+
+			targetElement.removeEventListener("mouseleave", leaveMesh);
+			targetElement.removeEventListener("mousemove", moveMesh);
+		}
+
+		function moveMesh(e) {
+			const targetElement = e.target.tagName !== "LI" ? e.target.closest("li") : e.target;
+			const parentNode = [...targetElement.parentElement.children];
+			const targetIndex = parentNode.indexOf(targetElement);
+
+			const plane = listArray[targetIndex];
+
 			pointer.x = (e.clientX / areaWidth) * 2 - 1;
 			pointer.y = -(e.clientY / areaHeight) * 2 + 1;
 			raycaster.setFromCamera(pointer, camera);
 
 			const intersected = raycaster.intersectObjects(listArray);
-			if (intersected[0]) {
-				const mesh = intersected[0].object;
-				const meshHoverUniform = mesh.material.uniforms.u_hover.value;
-				const meshPositionUniform = mesh.material.uniforms.u_position.value;
 
-				if (!meshHoverUniform.z) {
-					meshHoverUniform.x = intersected[0].uv.x;
-					meshHoverUniform.y = intersected[0].uv.y;
+			if (intersected[0]) {
+				const planeHoverUniform = plane.material.uniforms.u_hover.value;
+
+				if (!planeHoverUniform.z) {
+					planeHoverUniform.x = intersected[0].uv.x;
+					planeHoverUniform.y = intersected[0].uv.y;
 				}
-				mesh.userData.hoverTween && mesh.userData.hoverTween.kill();
-				mesh.userData.hoverTween = gsap.to(meshHoverUniform, 0.75, {
+
+				plane.userData.hoverTween && plane.userData.hoverTween.kill();
+				plane.userData.hoverTween = gsap.to(planeHoverUniform, 0.75, {
 					x: intersected[0].uv.x,
 					y: intersected[0].uv.y,
 					z: 1,
 					ease: "cubic.out",
 				});
 
-				mesh.position.hoverTween && mesh.position.hoverTween.kill();
-				mesh.position.hoverTween = gsap.to(meshPositionUniform, 0.75, {
-					x: mesh.position.x - intersected[0].point.x,
+				plane.position.hoverTween && plane.position.hoverTween.kill();
+				plane.position.hoverTween = gsap.to(plane.position, 0.75, {
+					x: plane.userData.position.x + (plane.userData.position.x - intersected[0].point.x) * (scaleUnit - 1),
 					ease: "cubic.out",
 				});
-
-				// intersected mesh가 currentMesh랑 같으면 필요한 부분만 업데이트하고 이후 처리는 무시
-				if (intersected[0].object === currentMesh) {
-					return;
-				}
-
-				const meshProgressUniform = mesh.material.uniforms.u_progress;
-
-				// 기존 currentMesh의 효과 제거
-				// 동일한 액션을 intersected mesh가 없을 때도 재사용하기위해 leaveMesh 함수로 별도 분리
-				currentMesh && leaveMesh(currentMesh);
-
-				currentMesh = mesh;
-				currentShader = mesh;
-				mesh.renderOrder = 1;
-
-				mesh.userData.progressTween && mesh.userData.progressTween.kill();
-				mesh.userData.progressTween = gsap.to(meshProgressUniform, 0.75, {
-					value: 0.99999,
-					ease: "cubic.out",
-				});
-
-				mesh.scale.hoverTween && mesh.scale.hoverTween.kill();
-				mesh.scale.hoverTween = gsap.to(mesh.scale, 0.75, {
-					x: scale * 1.2,
-					y: scale * 1.2,
-					ease: "cubic.out",
-				});
-			} else if (currentMesh) {
-				// currentMesh가 있으면
-				// 기존 currentMesh의 효과 제거
-				// 동일한 액션을 intersected mesh가 없을 때도 재사용하기위해 leaveMesh 함수로 별도 분리
-				leaveMesh(currentMesh);
-
-				currentMesh = null;
 			}
-		});
-
-		const popupMesh = document.querySelectorAll(".listitem a");
-		for (let i = 0; i < popupMesh.length; i++) {
-			popupMesh[i].addEventListener("click", popupShader, false);
 		}
 
-		function popupShader() {
-			currentShader.scale.clickTween && currentShader.scale.clickTween.kill();
-			currentShader.scale.clickTween = gsap.to(currentShader.scale, 0.75, {
-				x: scale * 6,
-				y: scale * 6,
+		function leaveMesh(e) {
+			const targetElement = e.target.tagName !== "LI" ? e.target.closest("li") : e.target;
+			const parentNode = [...targetElement.parentElement.children];
+			const targetIndex = parentNode.indexOf(targetElement);
+
+			const plane = listArray[targetIndex];
+			const planeProgressUniform = plane.material.uniforms.u_progress;
+			const planeHoverUniform = plane.material.uniforms.u_hover.value;
+
+			plane.renderOrder = 0;
+
+			plane.userData.hoverTween && plane.userData.hoverTween.kill();
+			plane.userData.hoverTween = gsap.to(planeHoverUniform, 0.5, {
+				x: 0,
+				y: 0,
+				z: 0,
 				ease: "cubic.out",
 			});
-		}
 
-		function leaveMesh(mesh) {
-			mesh.renderOrder = 0;
-			const meshHoverUniform = mesh.material.uniforms.u_hover.value;
-			const meshProgressUniform = mesh.material.uniforms.u_progress;
-			const meshPositionUniform = mesh.material.uniforms.u_position.value;
-			mesh.userData.hoverTween && mesh.userData.hoverTween.kill();
-			mesh.userData.hoverTween = gsap.to(meshHoverUniform, 0.5, { z: 0, ease: "cubic.out" });
+			plane.userData.progressTween && plane.userData.progressTween.kill();
+			plane.userData.progressTween = gsap.to(planeProgressUniform, 0.5, {
+				value: 0,
+				ease: "cubic.out",
+				onComplete: function () {
+					plane.material.uniforms.u_alpha.value = 1.0;
+				},
+			});
 
-			mesh.userData.progressTween && mesh.userData.progressTween.kill();
-			mesh.userData.progressTween = gsap.to(meshProgressUniform, 0.5, { value: 0, ease: "cubic.out" });
+			plane.scale.hoverTween && plane.scale.hoverTween.kill();
+			plane.scale.hoverTween = gsap.to(plane.scale, 0.5, {
+				x: plane.userData.scale.x,
+				y: plane.userData.scale.y,
+				ease: "cubic.out",
+			});
 
-			mesh.scale.hoverTween && mesh.scale.hoverTween.kill();
-			mesh.scale.hoverTween = gsap.to(mesh.scale, 0.5, { x: scale, y: scale, ease: "cubic.out" });
+			plane.position.hoverTween && plane.position.hoverTween.kill();
+			plane.position.hoverTween = gsap.to(plane.position, 0.5, {
+				x: plane.userData.position.x,
+				ease: "cubic.out",
+			});
 
-			mesh.position.hoverTween && mesh.position.hoverTween.kill();
-			mesh.position.hoverTween = gsap.to(meshPositionUniform, 0.5, { x: 0, ease: "cubic.out" });
+			targetElement.removeEventListener("mouseleave", leaveMesh);
+			targetElement.removeEventListener("mousemove", moveMesh);
 		}
 
 		gsap.ticker.add(animate);
@@ -383,6 +414,30 @@ const threeProject = (() => {
 			renderer.render(scene, camera);
 		}
 	};
+
+	const closeBtn = document.querySelector(".btn-menu");
+	closeBtn.addEventListener("click", function () {
+		document.body.style.overflow = "visible";
+
+		targetMesh.scale.clickTween && targetMesh.scale.clickTween.kill();
+		targetMesh.scale.clickTween = gsap.to(targetMesh.scale, 0.5, {
+			x: targetMesh.userData.scale.x,
+			y: targetMesh.userData.scale.y,
+			ease: "cubic.out",
+			onComplete: function () {
+				targetMesh.renderOrder = 0;
+				targetMesh.material.uniforms.u_alpha.value = 0.0;
+				targetMesh = null;
+			},
+		});
+
+		targetMesh.position.hoverTween && targetMesh.position.hoverTween.kill();
+		targetMesh.position.hoverTween = gsap.to(targetMesh.position, 0.5, {
+			x: targetMesh.userData.position.x,
+			y: targetMesh.userData.position.y,
+			ease: "cubic.out",
+		});
+	});
 
 	// gsap.ticker에서 render를 계속 실행하고 있어서 필요없음
 	// const setTheRender = () => {
@@ -402,19 +457,9 @@ const threeProject = (() => {
 		areaWidth = window.innerWidth;
 		areaHeight = window.innerHeight;
 		renderer.setSize(areaWidth, areaHeight);
-		renderer.setPixelRatio(devicePixelRatio);
-
-		scale = meshArea.offsetHeight / areaHeight;
-		for (let i = 0; i < listArray.length; i++) {
-			const item = listArray[i];
-
-			item.position.x = (meshWidth + boxGap.x) * scale * (i % listColumn);
-			item.position.y = -(meshHeight + boxGap.y) * scale * Math.floor(i / listColumn);
-			item.savePosition = item.position.clone();
-			item.scale.set(scale, scale);
-		}
-
-		setViewOffsetFunction();
+		renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
+		setTheSizing();
+		onScroll();
 		camera.aspect = areaWidth / areaHeight;
 		camera.updateProjectionMatrix();
 		renderRequest();
@@ -461,7 +506,6 @@ const threeProject = (() => {
 		setTheRenderer();
 		setTheCamera();
 		setTheLight();
-		// setTheModel();
 		setTheLayout();
 		setTheSizing();
 		setTheTexture();
